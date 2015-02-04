4c182b1077e6d2322e0b63f9649bb4e0d15ace4e
Hendrik Lammers <lammers@gmail.com>
2015-02-04 21:10:10 +0100

    Create prototype of colorlist

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', [])
+        .controller('ColorListController', function() {
+            var vm = this;
+
+            vm.prefix = 'color-';
+
+            vm.colors = [
+                {hex: '#3498db', name: 'blue'},
+                {hex: '#16a085', name: 'green'},
+                {hex: '#c0392b', name: 'red'},
+                {hex: '#f1c40f', name: 'yellow'},
+                {hex: '#8e44ad', name: 'purple'}
+            ];
+        });
+
+}());
