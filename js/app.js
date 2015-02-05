ac4a1c55b1dc962b7ce2f198a1035a65e2390a56
Hendrik Lammers <lammers@gmail.com>
2015-02-05 23:18:42 +0100

    Create sass variables and show them in dialog

--- js/app.js
+++ js/app.js
@@ -2,11 +2,48 @@
     'use strict';
 
     angular
-        .module('app', ['ui.sortable'])
-        .controller('ColorListController', function() {
+        .module('app', ['ui.sortable', 'ngDialog'])
+
+        .factory('sassService', function() {
+            var sassVariables = '',
+                service = {
+                    createSass: createSass,
+                    getSass: getSass
+                };
+
+            return service;
+
+            function createSass(colors, prefix) {
+                var result = '';
+
+                colors.forEach(function(elem) {
+                    var color = prefix + elem.name + ': ' + elem.hex + ';\n';
+
+                    result += color;
+                });
+
+                sassVariables = result;
+
+                return result;
+            }
+
+            function getSass() {
+                return sassVariables;
+            }
+        })
+
+        .controller('ResultDialogController', ['$scope', 'sassService', function($scope, sassService) {
+            $scope.result = sassService.getSass();
+        }])
+
+        .controller('ColorListController', ['ngDialog', 'sassService', function(ngDialog, sassService) {
             var vm = this;
 
             vm.prefix = 'color-';
+
+            vm.sortableOptions = {
+                handle: '.handle'
+            };
 
             vm.colors = [
                 {hex: '#3498db', name: 'blue'},
@@ -19,6 +56,15 @@
             vm.removeColor = function(index) {
                 vm.colors.splice(index, 1);
             };
-        });
+
+            vm.createSass = function() {
+                sassService.createSass(vm.colors, vm.prefix);
+
+                ngDialog.open({
+                    template: 'js/resultdialog.html',
+                    controller: 'ResultDialogController'
+                });
+            };
+        }]);
 
 }());
