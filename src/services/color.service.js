4b94e50dc4ba589a3ec75f055b9d2c891b1b01d1
Hendrik Lammers <lammers@gmail.com>
2015-02-16 21:36:33 +0100

    Update color service with hex/rgb conversion

--- src/services/color.service.js
+++ src/services/color.service.js
@@ -7,16 +7,45 @@
 
     /* @ngInject */
     function colorService() {
+        var colorStorage = [];
+
         var service = {
             updateColors: updateColors
         };
 
         return service;
 
+        /**
+         * Update colors stored in the service
+         * @param {array} colors
+         */
         function updateColors(colors) {
             colors.forEach(function(c) {
-                console.log(c);
+                var color = {
+                    rgb: createRgbString(c),
+                    hex: createHexString(c)
+                };
+
+                colorStorage.push(color);
             });
+        }
+
+        /**
+         * Creates rgb(255, 255, 255) string
+         * @param {array} rgb [123, 38, 8] color values
+         * @return {string}
+         */
+        function createRgbString(rgb) {
+            return 'rgb(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ')';
+        }
+
+        /**
+         * Creates #FFFFFF string
+         * @param {array} rgb [123, 38, 8] color values
+         * @return {string}
+         */
+        function createHexString(rgb) {
+            return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
         }
     }
 
