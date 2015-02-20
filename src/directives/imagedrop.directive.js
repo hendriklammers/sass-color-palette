d26d34ff9b0a9389aa49085142de9f217199710d
Hendrik Lammers <lammers@gmail.com>
2015-02-20 07:48:02 +0100

    Update imagedrop directive

--- src/directives/imagedrop.directive.js
+++ src/directives/imagedrop.directive.js
@@ -9,28 +9,27 @@
     function imageDrop(colorService) {
         var directive = {
             restrict: 'EA',
-            template: '<div class="imagedrop"><span class="imagedrop-label">Drag an image in this area</div>',
+            templateUrl: 'src/templates/imagedrop.html',
             replace: true,
             link: link
         };
 
         return directive;
 
+        /**
+         * Directive link function
+         * @param scope
+         * @param elem
+         */
         function link(scope, elem) {
             var colorThief = new ColorThief();
 
-            // Disable default drag/drop browser behaviour
-            window.addEventListener('dragover', function(e){
-                e = e || event;
-                e.preventDefault();
-            }, false);
-            window.addEventListener('drop', function(e){
-                e = e || event;
-                e.preventDefault();
-            }, false);
+            disableDefault();
 
-            // Listen for drop event on directive element
+            // Listen for drop events on directive element
             elem[0].addEventListener('drop', handleFileDrop);
+            elem[0].addEventListener('dragenter', handleDragEnter);
+            elem[0].addEventListener('dragleave', handleDragLeave);
 
             /**
              * Gets triggerd when user drops file in dropzone
@@ -38,6 +37,8 @@
              */
             function handleFileDrop(event) {
                 event.preventDefault();
+
+                scope.isBusy = true;
 
                 for (var i = 0; i < event.dataTransfer.files.length; i++) {
                     var file = event.dataTransfer.files[i];
@@ -50,9 +51,25 @@
 
                         reader.readAsDataURL(file);
                     } else {
+                        scope.isBusy = false;
+
                         window.alert('Filetype not supported.');
                     }
                 }
+            }
+
+            /**
+             * Add class when user drags file over dropzone
+             */
+            function handleDragEnter() {
+                elem[0].classList.add('dragging');
+            }
+
+            /**
+             * Remove class when user drags file out of dropzone
+             */
+            function handleDragLeave() {
+                elem[0].classList.remove('dragging');
             }
 
             /**
@@ -68,9 +85,26 @@
                     var colors = colorThief.getPalette(img, 100);
 
                     colorService.updateColors(colors);
+
+                    scope.isBusy = false;
+                    scope.$apply();
                 };
 
                 img.src = event.target.result;
+            }
+
+            /**
+             * Disables default drag/drop browser behavior
+             */
+            function disableDefault() {
+                window.addEventListener('dragover', function(e){
+                    e = e || event;
+                    e.preventDefault();
+                }, false);
+                window.addEventListener('drop', function(e){
+                    e = e || event;
+                    e.preventDefault();
+                }, false);
             }
         }
     }
